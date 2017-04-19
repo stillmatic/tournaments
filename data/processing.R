9868201ed631dd5d04ade8a9d8986cf20dc5f0e6
Chris Hua <hua.christopher@gmail.com>
2017-04-19 16:19:16 -0400

    add MLE probs, presentation start

--- data/processing.R
+++ data/processing.R
@@ -17,20 +17,6 @@ coef(ndca_fit) %>%
     ggtitle("Estimated coefficients from BT model", "From NDCA pairwise comparisons")
 
 # 2009-2010 data
-data09 <- jsonlite::fromJSON("~/code/tournament/data/2009-2010.json")
-# rounds09 <- do.call(rbind, data09$debaters)
-# judges09 <- purrr::map(data09$judge, rep, each = 4) 
-# names(judges09) <- c("judge_school", "judge_ID")
-# cbind(judges09, rounds09) %>% head
-
-# data09$debaters %>% magrittr::extract2(1) %>% 
-#     rename(speaks = `speaker points`) %>%
-#     group_by(position) %>%
-#     dplyr::summarize(id = stringr::str_c(ID, collapse = ","), 
-#                      speaks = base::sum(as.numeric(speaks))) %>%
-#     ungroup %>%
-#     summarize(aff = first(id), aff_speaks = first(speaks), 
-#               neg = last(id), neg_speaks = last(speaks))
 
 clean <- function(x) {
     x %>% rename(speaks = `speaker points`) %>%
@@ -41,6 +27,8 @@ clean <- function(x) {
         summarize(aff = first(id), aff_speaks = first(speaks), 
                   neg = last(id), neg_speaks = last(speaks))
 }
+
+data09 <- jsonlite::fromJSON("~/code/tournament/data/2009-2010.json")
 
 pairwise09 <- purrr::map_df(data09$debaters, clean)
 pairwise09$win <- data09$win
@@ -60,3 +48,26 @@ fit_09$pi %>% purrr::map(hist)
 # btm_fit_09 <- BradleyTerry2::BTm(outcome = pairwise09$win, 
                    # player1 = factor(pairwise09$aff), 
                    # player2 = factor(pairwise09$neg), br = TRUE)
+
+## --
+
+data10 <- jsonlite::fromJSON("~/code/tournament/data/2010-2011.json")
+
+pairwise10 <- purrr::map_df(data10$debaters, clean)
+pairwise10$win <- data10$win
+# pairwise10 %<>%
+    # filter(aff != "28417,28417", neg != "28417,28417")
+write.csv(pairwise10, "~/code/tournament/data/pairs10.csv")
+pairwise_mat_10 <- pairwise10 %>%
+    dplyr::select(aff, neg, win) %>%
+    mutate(aff = factor(aff), neg = factor(neg), win = as.numeric(win)) %>%
+    BradleyTerryScalable::pairs_to_matrix()
+
+fit_10 <- BradleyTerryScalable::btfit(
+    pairwise_mat_10, 1.5, maxit = 1000000,
+    components = connected_components(pairwise_mat_10)$components)
+
+probs_10 <- fit_10$pi %>% unlist
+probs_09 <- fit_09$pi %>% unlist
+probs_10 %>% write.csv("~/code/tournament/data/probs10.csv")
+probs_09 %>% write.csv("~/code/tournament/data/probs09.csv")
